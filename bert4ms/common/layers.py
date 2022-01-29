5ca85ffbef706d678d45d5b380eb71099e5bf1f3
lvyufeng <lvyufeng@cqu.edu.cn>
2022-01-30 06:44:05 +0800

    add distilbert, transfo_xl, xlm and xlnet placeholder

--- bert4ms/common/layers.py
+++ bert4ms/common/layers.py
@@ -1,6 +1,8 @@
 import math
 import mindspore
 import mindspore.nn as nn
+import mindspore.ops as ops
+import mindspore.numpy as mnp
 from mindspore.common.initializer import initializer, Normal, Uniform, HeUniform, _calculate_fan_in_and_fan_out
 from mindspore import Tensor
 
@@ -26,3 +28,57 @@ class Embedding(nn.Embedding):
         embedding = cls(rows, cols, embedding_table=embeddings, padding_idx=padding_idx)
         embedding.embedding_table.requires_grad = not freeze
         return embedding
+
+class SequenceSummary(nn.Cell):
+    def __init__(self, config):
+        super(SequenceSummary, self).__init__()
+
+        self.summary_type = config.summary_type if hasattr(config, 'summary_use_proj') else 'last'
+        if self.summary_type == 'attn':
+            # We should use a standard multi-head attention module with absolute positional embedding for that.
+            # Cf. https://github.com/zihangdai/xlnet/blob/master/modeling.py#L253-L276
+            # We can probably just use the multi-head attention module of PyTorch >=1.1.0
+            raise NotImplementedError
+
+        self.summary = ops.Identity()
+        if hasattr(config, 'summary_use_proj') and config.summary_use_proj:
+            if hasattr(config, 'summary_proj_to_labels') and config.summary_proj_to_labels and config.num_labels > 0:
+                num_classes = config.num_labels
+            else:
+                num_classes = config.hidden_size
+            self.summary = Dense(config.hidden_size, num_classes)
+
+        self.activation = ops.Identity()
+        if hasattr(config, 'summary_activation') and config.summary_activation == 'tanh':
+            self.activation = nn.Tanh()
+
+        self.first_dropout = ops.Identity()
+        if hasattr(config, 'summary_first_dropout') and config.summary_first_dropout > 0:
+            self.first_dropout = nn.Dropout(config.summary_first_dropout)
+
+        self.last_dropout = ops.Identity()
+        if hasattr(config, 'summary_last_dropout') and config.summary_last_dropout > 0:
+            self.last_dropout = nn.Dropout(1-config.summary_last_dropout)
+
+    def construct(self, hidden_states, cls_index=None):
+        if self.summary_type == 'last':
+            output = hidden_states[:, -1]
+        elif self.summary_type == 'first':
+            output = hidden_states[:, 0]
+        elif self.summary_type == 'mean':
+            output = hidden_states.mean(axis=1)
+        elif self.summary_type == 'cls_index':
+            if cls_index is None:
+                cls_index = mnp.full_like(hidden_states[..., :1, :], hidden_states.shape[-2]-1, dtype=mindspore.int32)
+            else:
+                cls_index = cls_index.expand_dims(-1).expand_dims(-1)
+                cls_index = mnp.broadcast_to(cls_index, (-1,) * (cls_index.ndim-1) + (hidden_states.shape[-1],))
+            # shape of cls_index: (bsz, XX, 1, hidden_size) where XX are optional leading dim of hidden_states
+            output = ops.gather_d(hidden_states, -2, cls_index).squeeze(-2) # shape (bsz, XX, hidden_size)
+
+        output = self.first_dropout(output)
+        output = self.summary(output)
+        output = self.activation(output)
+        output = self.last_dropout(output)
+
+        return output
---
+++ bert4ms/models/xlnet.py
@@ -0,0 +1,5 @@
+PRETRAINED_MODEL_ARCHIVE_MAP = {
+}
+
+PYTORCH_PRETRAINED_MODEL_ARCHIVE_LIST = ["xlnet-base-cased", "xlnet-large-cased"]
+
