487e024bc96726369aae789f60935051530f43d8
lvyufeng <lvyufeng@cqu.edu.cn>
2022-08-11 09:58:46 +0800

    use ops.LpNorm instead of self-defined norm

--- cybertron/common/modules/ops.py
+++ cybertron/common/modules/ops.py
@@ -207,51 +207,61 @@ def bmm(x, y, transpose_x=False, transpose_y=False):
 
 def masked_fill_(inputs:Tensor, mask:Tensor, value:float):
     # return ops.masked_fill(inputs, mask, value)
-    return inputs.masked_fill(mask, value)
+    return input
 
-def norm(x, ord=None, axis=None, keepdims=False):
-    # Immediately handle some default, simple, fast, and common cases.
-    ndim = x.ndim
+
+@constexpr
+def _check_axis(axis, ord, ndim):
     if axis is None:
         if ((ord is None) or
             (ord in ('f', 'fro') and ndim == 2) or
             (ord == 2 and ndim == 1)):
+            return None, True
+        else:
+            axis = tuple(range(ndim))
+            return axis, False
+    else:
+        if isinstance(axis, int):
+            axis = (axis,)
+        elif isinstance(axis, tuple):
+            if len(axis) > 2:
+                raise ValueError("Improper number of dimensions to norm.")
+        else:
+            raise ValueError(f'axis should be int or tuple but got {type(axis)}')
+        return axis, False
 
-            x = x.ravel()
-            sqnorm = dot(x, x)
-            ret = sqrt(sqnorm)
-            if keepdims:
-                ret = ret.reshape(ndim*[1])
-            return ret
-
-    # Normalize the `axis` argument to a tuple.
-    if axis is None:
-        axis = tuple(range(ndim))
-    elif not isinstance(axis, tuple):
-        # try:
-        #     axis = int(axis)
-        # except Exception as e:
-        #     raise TypeError("'axis' must be None, an integer or a tuple of integers") from e
-        axis = (axis,)
-    if len(axis) > 2:
-        raise_value_error("Improper number of dimensions to norm.")
-
+@constexpr
+def _check_ord(ord, axis):
     if len(axis) == 1:
         if isinstance(ord, str):
-            raise_value_error(f"Invalid norm order '{ord}' for vectors")
+            raise ValueError(f"Invalid norm order '{ord}' for vectors")
+    elif len(axis) == 2:
+        if ord not in [2, -2, 1, -1, inf, -inf, 'fro', 'f', 'nuc', None]:
+            raise ValueError("Invalid norm order for matrices.")
+
+def norm(x, ord=None, axis=None, keepdims=False):
+    ndim = x.ndim
+    # Normalize the `axis` argument to a tuple.
+    axis, immediate = _check_axis(axis, ord, ndim)
+    _check_ord(ord, axis)
+    # Immediately handle some default, simple, fast, and common cases.
+    if immediate:
+        x = x.ravel()
+        sqnorm = dot(x, x)
+        ret = sqrt(sqnorm)
+        if keepdims:
+            ret = ret.reshape(ndim*[1])
+        return ret
+
+    if isinstance(ord, int):
+        return ops.LpNorm(axis, ord, keepdims)
+
+    if len(axis) == 1:
         if ord == inf:
             return ops.abs(x).max(axis=axis, keepdims=keepdims)
         elif ord == -inf:
             return ops.abs(x).min(axis=axis, keepdims=keepdims)
-        elif ord == 0:
-            # Zero norm
-            return (x != 0).astype(x.dtype).sum(axis=axis, keepdims=keepdims)
-        elif ord == 1:
-            # special case for speedup
-            # reduce_sum = _get_cache_prim(ops.ReduceSum)(keepdims)
-            reduce_sum = ops.ReduceSum(keepdims)
-            return ops.reduce_sum(ops.abs(x), axis=axis)
-        elif ord is None or ord == 2:
+        elif ord is None:
             # special case for speedup
             # conj = _get_cache_prim(ops.Conj)()
             conj = ops.Conj()
@@ -277,25 +287,12 @@ def norm(x, ord=None, axis=None, keepdims=False):
         col_axis = normalize_axis_index(col_axis, ndim)
         if row_axis == col_axis:
             raise_value_error('Duplicate axes given.')
-        if ord not in [2, -2, 1, -1, inf, -inf, 'fro', 'f', 'nuc', None]:
-            raise_value_error("Invalid norm order for matrices.")
 
-        if ord == 2:
-            ret =  _multi_svd_norm(x, row_axis, col_axis, 'amax')
-        elif ord == -2:
-            ret = _multi_svd_norm(x, row_axis, col_axis, 'amin')
-        elif ord == 1:
-            if col_axis > row_axis:
-                col_axis -= 1
-            ret = ops.reduce_sum(abs(x), axis=row_axis).max(axis=col_axis)
-        elif ord == inf:
+    
+        if ord == inf:
             if row_axis > col_axis:
                 row_axis -= 1
             ret = ops.reduce_sum(abs(x), axis=col_axis).max(axis=row_axis)
-        elif ord == -1:
-            if col_axis > row_axis:
-                col_axis -= 1
-            ret = ops.reduce_sum(abs(x), axis=row_axis).min(axis=col_axis)
         elif ord == -inf:
             if row_axis > col_axis:
                 row_axis -= 1
@@ -361,7 +358,7 @@ def clip_grad_norm(grads, max_norm: float, norm_type: float = 2.0, error_if_nonf
         norms = ()
         for grad in grads:
             norms += (norm(grad, norm_type),)
-        total_norm = norm(ops.stack(), norm_type)
+        total_norm = norm(ops.stack(norms), norm_type)
 
     if error_if_nonfinite and ops.logical_or(ops.isnan(total_norm), ops.bool_not(ops.isfinite(total_norm))):
         raise_runtime_error(
