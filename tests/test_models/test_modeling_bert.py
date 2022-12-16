5563e33ac8eeadf48a794b184857461c58d62a8a
lvyufeng <lvyufeng@cqu.edu.cn>
2022-12-16 10:49:03 +0800

    add ut of BertForSequenceClassification

--- tests/test_models/test_modeling_bert.py
+++ tests/test_models/test_modeling_bert.py
@@ -83,3 +83,25 @@ class TestModelingBert(unittest.TestCase):
 
         assert np.allclose(outputs.asnumpy(), outputs_pt.detach().numpy(), atol=1e-5)
         assert np.allclose(pooled.asnumpy(), pooled_pt.detach().numpy(), atol=1e-5)
+
+    def test_modeling_bert_sequence_with_ckpt(self):
+        from cybertron import BertForSequenceClassification
+        from transformers import BertForSequenceClassification as ptBertForSequenceClassification
+        context.set_context(mode=context.GRAPH_MODE)
+        model = BertForSequenceClassification.load('bert-base-uncased')
+        model.set_train(False)
+        input_ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] + [0] * 500
+
+        
+        pt_model = ptBertForSequenceClassification.from_pretrained('bert-base-uncased')
+        pt_model.eval()
+
+        model.classifier.weight.set_data(mindspore.Tensor(pt_model.classifier.weight.detach().numpy()))
+        model.classifier.bias.set_data(mindspore.Tensor(pt_model.classifier.bias.detach().numpy()))
+
+        ms_input_ids = Tensor(input_ids, mindspore.int32).reshape(1, -1)
+        (outputs, ) = model(ms_input_ids)
+        pt_input_ids = torch.IntTensor(input_ids).reshape(1, -1)
+        (outputs_pt,) = pt_model(input_ids=pt_input_ids)
+
+        assert np.allclose(outputs.asnumpy(), outputs_pt.detach().numpy(), atol=1e-5)
